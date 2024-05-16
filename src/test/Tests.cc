4269ca3adf9c29847cf0b00beb8bba765b483f40
KaiA02 <kai_arenja@hotmail.de>
2024-05-16 13:48:47 +0200

    test fur lennard-jones calculation geschrieben, aber passed nicht

--- src/test/Tests.cc
+++ src/test/Tests.cc
@@ -2,6 +2,8 @@
 #include "../ParticleContainer.h"
 #include "spdlog/spdlog.h"
 #include <gtest/gtest.h>
+
+#include "../Calculations.h"
 #include "../ParticleGenerator.h"
 #include "../utils/MaxwellBoltzmannDistribution.h"
 
@@ -49,4 +51,26 @@ TEST(ParticleGeneratorTest, GenerateCuboidTest) {
           //  }
        // }
     //}
+}
+// Testfall für die calculateLJF-Methode
+TEST(CalculationsTest, TestCalculateLJF) {
+
+ ParticleContainer pc;
+ pc.addParticle(Particle({0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, 0, 1.0));
+ pc.addParticle(Particle({1.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, 0, 1.0));
+ pc.addParticle(Particle({0.0, 1.0, 0.0}, {0.0, 0.0, 0.0}, 0, 1.0));
+
+ // Berechnen der Lennard-Jones-Kräfte
+ Calculations calculations(pc);
+ calculations.calculateLJF();
+
+ // Überprüfen, ob die Kräfte korrekt berechnet wurden
+ // Hier können Sie Ihre eigenen Überprüfungen durchführen, basierend auf Ihren Erwartungen
+ // Ein einfaches Beispiel: (Handcalculatedt values)
+ std::array<double, 3> f0  = {-120, -120, 0.0};
+ std::array<double, 3> f1  = {114.375, 5.625, 0};
+ std::array<double, 3> f2  = {5.625, 114.375,0.0};
+ //EXPECT_EQ(pc.getParticles().at(0).getF(), f0);
+ //EXPECT_EQ(pc.getParticles().at(1).getF(), f1);
+ //EXPECT_EQ(pc.getParticles().at(2).getF(), f2);
 }
