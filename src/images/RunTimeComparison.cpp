adbf3cb764bb4e0f8906b442cf5b5a744cc715f9
KaiA02 <kai_arenja@hotmail.de>
2024-06-03 17:33:53 +0200

    versucht runtime_comparison.png in doxygen reinzutun

---
+++ src/images/RunTimeComparison.cpp
@@ -0,0 +1,10 @@
+
+/**
+ * @brief Compares the runtime per iteration for different implementations.
+ *
+ * This graph shows the measured times for simulating a 2D-square
+ * with different numbers of molecules (1000, 2000, 4000, 8000).
+ *
+ * ![Runtime Comparison](runtime_comparison.png)
+ */
+void runtime();
