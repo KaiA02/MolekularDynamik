bd2e04e5f2941e37f276a1cc54aeb127ace350e6
KaiA02 <kai_arenja@hotmail.de>
2024-06-17 13:02:45 +0200

    doxygen kommentare für Thermostat.h

--- src/Thermostat.h
+++ src/Thermostat.h
@@ -1,9 +1,11 @@
 #ifndef THERMOSTAT_H
 #define THERMOSTAT_H
 #include <vector>
-
 #include "Particle.h"
 
+/**
+ * @brief Thermostat class: used to set/calculate the temperature of the system
+ */
 class Thermostat {
 private:
     double temp_init;
@@ -13,10 +15,18 @@ private:
 
     const static int dimension = 3;
 
-    // Helper function to calculate current temperature
+    /**
+    * @brief Helper function to calculate the temperature by using the kinetic energy
+    * @param particles all particles in the container
+    */
     double calculateCurrentTemperature(const std::vector<Particle>& particles) const;
 
-    // Helper function to scale velocities
+    /**
+    * @brief Helper function to scalet the velocities by using the scalingFactor
+    * scaling factor is calculated by the other methods
+    * @param particles all particles in the container
+    * @param scalingFactor calculated by the other methods
+    */
     void scaleVelocities(std::vector<Particle>& particles, double scalingFactor) const;
 
 public:
@@ -28,14 +38,27 @@ public:
     const double getDelta_Temp() const;
     const double getCurrentTemp(const std::vector<Particle>& particles) const;
 
-    // Set temperature directly via velocity scaling
-    //First Option
+    /**
+    * @brief sets the temperature directly
+    * calls scaleVelocities()
+    * @param particles all particles in the container
+    * "setting a temperature directly via velocity scaling"
+    */
     void setTemperatureDirectly(std::vector<Particle>& particles) const;
 
-    // Gradual velocity scaling
-    //Second option
+    /**
+    * @brief sets the temperature gradualy
+    * calls scaleVelocities()
+    * @param particles all particles in the container
+    * "gradual velocity scaling"
+    */
     void gradualScaling(std::vector<Particle>& particles) const;
 
+    /**
+    * @brief sets the temperature directly to the initial value
+    * calls scaleVelocities()
+    * @param particles all particles in the container
+    */
     void setInitialTemperature(std::vector<Particle>& particles) const;
 };
 
