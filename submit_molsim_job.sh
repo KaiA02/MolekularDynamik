404707a05e3fcc9d98a03541a00ee4225ea6f812
KaiA02 <kai_arenja@hotmail.de>
2024-06-19 12:52:06 +0200

    job.sh für linux cluster

---
+++ submit_molsim_job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=my_molsim_job         # Name des Jobs
+#SBATCH --output=my_molsim_job_out.txt    # Datei für Standardausgabe
+#SBATCH --error=my_molsim_job_err.txt     # Datei für Fehlerausgabe
+#SBATCH --time=04:00:00                  # Maximale Laufzeit (Stunden:Minuten:Sekunden)
+#SBATCH --partition=serial_std           # Partition wählen (hier: serial_std)
+#SBATCH --mem=4gb                        # Speicher pro Job
+#SBATCH --cpus-per-task=1                # Anzahl der CPUs pro Job
+#SBATCH --mail-type=END                  # E-Mail-Benachrichtigung bei Jobende
+#SBATCH --mail-user=ge47jow@tum.de # Ihre E-Mail-Adresse für Benachrichtigungen
+
+# Laden der benötigten Module
+module load gcc
+module load cmake
+
+# Navigieren zum Verzeichnis, in dem das Programm kompiliert wird
+cd /home/MolekularDynamik
+mkdir build
+cd build
+
+# Kompilieren des Programms mit gprof
+cmake ..
+make
+gcc -pg -o MolSim ../input/eingabe-equilibrium.xml
+
+# Ausführen des Programms mit einem XML-Eingabedateiargument
+./MolSim ../input/eingabe-equilibrium.xml
+
+# Optional: Speichern der gmon.out-Datei für das Profiling
+cp gmon.out /home/MolekularDynamik/build/gmon.out
