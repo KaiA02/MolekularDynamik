9e9a0f0962ae4c279dbdd2221d0dafea90864c9f
Kai <109000544+KaiA02@users.noreply.github.com>
2024-07-05 11:38:13 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -5,11 +5,11 @@
 #SBATCH --get-user-env
 #SBATCH --clusters=cm2_tiny
 #SBATCH --partition=cm2_tiny
-#SBATCH --mem=200MB
+#SBATCH --mem=150MB
 #SBATCH --get-user-env
 #SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
 #SBATCH --mail-user=ge47jow@mytum.de
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 
 ./MolSim ../input/eingabe-disk.xml 
