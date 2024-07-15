5ca0abfcf145ce1028f7d224e26ebcd776a24ff6
Kai <109000544+KaiA02@users.noreply.github.com>
2024-07-15 11:51:48 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-#SBATCH -J "name"
+#SBATCH -J Task3
 #SBATCH -o ./%x.%j.%N.out
-#SBATCH -D ./MolekularDynamik/build
+#SBATCH -D ./build
 #SBATCH --get-user-env
-#SBATCH --clusters=cm2_tiny
-#SBATCH --partition=cm2_tiny
-#SBATCH --mem=200MB
-#SBATCH --get-user-env
+#SBATCH --clusters=cm2
 #SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
 #SBATCH --mail-user=kai_arenja@hotmail.de
 #SBATCH --mail-type=end
-#SBATCH --time=00:30:00
+#SBATCH --time=30:00:00
 
-./MolSim ../input/"name".xml 
+./MolSim ../input/eingabe-Rayleigh-Taylor-3D.xml 
